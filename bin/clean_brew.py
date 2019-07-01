83ee5d9bf86b31af2f51f224c1c3fe640bd10c6d
Darryl Forbes <darryl.forbes@u.northwestern.edu>
2019-07-01 01:06:29 -0500

    Updated clean_brew.py

--- bin/clean_brew.py
+++ bin/clean_brew.py
@@ -7,4 +7,11 @@ leaves = subprocess.check_output(['brew', 'leaves'])
 for leaf in leaves.splitlines():
     print(leaf.decode('UTF-8'))
 
+print("")
+
+deps = subprocess.check_output(['brew', 'deps', '--installed'])
+
+for dep in deps.splitlines():
+    print(dep.decode('UTF-8'))
+
 # Compare dependencies of leaves with installed packages to find orphans
