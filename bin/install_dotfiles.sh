1244453f3abdabddd65eada7bdaeed85d978ee3f
Darryl Forbes <darryl.forbes@u.northwestern.edu>
2019-08-12 11:17:04 -0500

    Fixed typo

--- bin/install_dotfiles.sh
+++ bin/install_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Credits: https://medium.com/@satorusasozaki/manage-your-dotfiles-on-github-8f8a2a57c85
 
-ln -sf ~/dofiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
