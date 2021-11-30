7faa4cb4e4dcac1ee4d057ee55bd0304c41dbb41
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-11-30 14:56:58 -0600

    Remove .ideavimrc symlink from install script

--- bin/install_dotfiles.sh
+++ bin/install_dotfiles.sh
@@ -4,10 +4,8 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/bin ~/bin
-
